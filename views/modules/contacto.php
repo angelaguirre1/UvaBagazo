7df045360ec93cbda920707f1bda43a088f389db
MrMKm <50241192+MrMKm@users.noreply.github.com>
2019-05-04 21:50:02 -0500

    Update contacto.php

--- views/modules/contacto.php
+++ views/modules/contacto.php
@@ -13,83 +13,39 @@
     </section>
 
     <!-- Main content -->
+    <body>
+
+    <div class="container">
+    <!--la propiedad enctype permite el envío de archivos adjuntos en el formulario. -->    
+      <form id="form1" class="well col-lg-12" action="enviar.php" method="post" name="form1" enctype="multipart/form-data">
+        <div class="row">
+        <div class="col-lg-6">
+          <label>Nombre*</label> <input id="Nombre" class="form-control" type="text" name="Nombre" /> 
+          <label>Email*</label> <input id="Email" class="form-control" type="email" name="Email" />
+        </div>
+          <div class="col-lg-6"><label>Mensaje*</label> 
+          <textarea id="Mensaje" class="form-control" name="Mensaje" rows="4"></textarea>
+          </div>
+          <div class="col-lg-12">
+      <label for="exampleInputFile">Adjuntar archivo</label>
+      <input type="file" name="adjunto" id="archivo-adjunto">
+      <p class="help-block">Example block-level help text here.</p>
+    </div>
+          <button class="btn btn-default pull-right" type="submit">Enviar</button>
+        </div>
+      </form>
+    </div>
+
+      <!-- javascript para confirmar datos del formulario.
+      ================================================== -->
+      <!-- La página carga más rapido si estan situados al final del documento. -->
+      <script src="js/jquery.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+
+    </body>
     <section class="content">
         <div class="row">
-        <body>
-          <?php
-
-          $Nombre = $_POST['Nombre'];
-          $Email = $_POST['Email'];
-          $Mensaje = $_POST['Mensaje'];
-          $archivo = $_FILES['adjunto'];
-
-          if ($Nombre=='' || $Email=='' || $Mensaje==''){
-
-          echo "<script>alert('Los campos marcados con * son obligatorios');location.href ='javascript:history.back()';</script>";
-
-          }else{
-
-
-              require("class.phpmailer.php");
-              $mail = new PHPMailer();
-
-              $mail->From     = $Email;
-              $mail->FromName = $Nombre; 
-              $mail->AddAddress("correo@tucorreo.com"); // Dirección a la que llegaran los mensajes.
-            
-          // Aquí van los datos que apareceran en el correo que reciba
-              //adjuntamos un archivo 
-                  //adjuntamos un archivo
-                      
-              $mail->WordWrap = 50; 
-              $mail->IsHTML(true);     
-              $mail->Subject  =  "Contacto";
-              $mail->Body     =  "Nombre: $Nombre \n<br />".    
-              "Email: $Email \n<br />".    
-              "Mensaje: $Mensaje \n<br />";
-              $mail->AddAttachment($archivo['tmp_name'], $archivo['name']);
-              
-              
-              
-
-          // Datos del servidor SMTP
-
-              $mail->IsSMTP(); 
-              $mail->Host = "ssl://smtp.gmail.com:465";  // Servidor de Salida.
-              $mail->SMTPAuth = true; 
-              $mail->Username = "tucorreo@gmail.com";  // Correo Electrónico
-              $mail->Password = "tucontraseña"; // Contraseña
-              
-              if ($mail->Send())
-              echo "<script>alert('Formulario enviado exitosamente, le responderemos lo más pronto posible.');location.href ='javascript:history.back()';</script>";
-              else
-              echo "<script>alert('Error al enviar el formulario');location.href ='javascript:history.back()';</script>";
-
-          }
-
-          ?>
-
-          <label>Nombre*</label> <input id="Nombre" class="form-control" type="text" name="Nombre" /> 
-                  <label>Email*</label> <input id="Email" class="form-control" type="email" name="Email" />
-                </div>
-                  <div class="col-lg-6"><label>Mensaje*</label> 
-                  <textarea id="Mensaje" class="form-control" name="Mensaje" rows="4"></textarea>
-                  </div>
-                  <div class="col-lg-12">
-              <label for="exampleInputFile">Adjuntar archivo</label>
-              <input type="file" name="adjunto" id="archivo-adjunto">
-              <p class="help-block">Example block-level help text here.</p>
-            </div>
-                  <button class="btn btn-default pull-right" type="submit">Enviar</button>
-                </div>
-              </form>
-            </div>
-            
-              <!-- javascript para confirmar datos del formulario.
-              ================================================== -->
-              <!-- La página carga más rapido si estan situados al final del documento. -->
-              <script src="js/jquery.js"></script>
-              <script src="js/bootstrap.min.js"></script>
+          
         </div>
     </section>
   </div>
